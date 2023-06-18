7c4ea66971c81cb591d03033fb8c5c7edb3cebec
NilsRamstoeck <nilsramstoeck@gmail.com>
2023-06-18 13:16:49 +0200

    cleanup Desktop

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,26 +1,18 @@
-import { PlasmaWindow } from "@/components/PlasmaWindow";
+import { createWindow, PlasmaWindow } from "@/components/PlasmaWindow";
 import { WindowManagerContext } from "@/DesktopEnviroment";
 import { createContext, h } from "preact";
 import { PropsWithChildren } from "preact/compat";
-import { useContext, useRef } from "preact/hooks";
+import { useContext, useEffect, useRef } from "preact/hooks";
 
 type Props = {
 
 };
 
-export const DesktopDimensionContext = createContext({ w: 0, h: 0 });
+export function Desktop({ }: Props) {
 
-export function Desktop({ children }: PropsWithChildren<Props>) {
+  const [{ windows }] = useContext(WindowManagerContext);
 
-  const [{ windows }, dispatch] = useContext(WindowManagerContext);
-  const desktopElement = useRef<HTMLDivElement>();
-
-  return <DesktopDimensionContext.Provider value={{
-    w: desktopElement.current?.clientHeight ?? 0,
-    h: desktopElement.current?.clientWidth ?? 0
-  }}>
-    <div ref={desktopElement} className='plasma-desktop'>
-      {windows.map((props) => <PlasmaWindow key={props.id} {...props}></PlasmaWindow>)}
-    </div>
-  </DesktopDimensionContext.Provider>;
+  return <div className='plasma-desktop'>
+    {windows.map((props) => <PlasmaWindow key={props.id} {...props}></PlasmaWindow>)}
+  </div>;
 }
