531dfe95b92f8c3b1f4d47da18734ce8f2a7063d
NilsRamstoeck <nilsramstoeck@gmail.com>
2023-06-19 21:52:43 +0200

    removed programs from home menu

--- src/components/HomeMenu.tsx
+++ src/components/HomeMenu.tsx
@@ -3,7 +3,6 @@ import { faArrowsRotate, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { h } from "preact";
 import { useContext } from "preact/hooks";
-import * as Programs from '@/programs';
 
 type Props = {
 
@@ -17,13 +16,6 @@ export function HomeMenu({ }: Props) {
 
   return <div className='homemenu plasma-box-inline'>
     
-    {Object.entries(Programs)
-      .map(([_, program]) => <span className='plasma-button plasma-box-top' onClick={() => program.launch(windowManager)}>
-        <FontAwesomeIcon icon={program.icon}></FontAwesomeIcon>
-        <span className='plasma-center'>{program.name}</span>
-      </span>
-      )}
-
     <span className='plasma-button plasma-box-top' onClick={() => reboot()}>
       <FontAwesomeIcon icon={faArrowsRotate}></FontAwesomeIcon>
       <span className='plasma-center'>Reboot</span>
