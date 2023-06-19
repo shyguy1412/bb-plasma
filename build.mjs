9896eb26fae8d9d466522f17bed4c4a80d882f69
NilsRamstoeck <nilsramstoeck@gmail.com>
2023-06-19 21:51:06 +0200

    create plugin to load core scripts as text

--- build.mjs
+++ build.mjs
@@ -1,10 +1,43 @@
-import { context } from 'esbuild';
+import { context, build } from 'esbuild';
+import { gzip } from 'node-gzip';
 
 const WATCH = process.argv.includes('--watch');
+
+/** @type import('esbuild').Plugin*/
+const netscriptLoaderPlugin = {
+  name: 'NetscriptLoader',
+  setup: function (pluginBuild) {
+    pluginBuild.onLoad({ filter: /.*\.ns\.ts[x]?$/ }, async (args) => {
+
+      const { path } = args;
+
+      const script = (await build({
+        entryPoints: [path],
+        write: false,
+        bundle: true,
+        minify: !WATCH,
+        format: 'esm',
+        platform: 'browser',
+        logLevel: 'info',
+        loader: { '.svg': 'text', '.css': 'text' },
+        external: ['bb-plasma'],
+        alias: {
+          'react': 'preact/compat'
+        },
+      })).outputFiles[0].text;
+
+      return {
+        contents: Buffer.from(script).toString('base64'),
+        loader: 'text'
+      };
+    });
+  }
+};
 
 const ctx = await context({
   entryPoints: ['./src/main.tsx'],
   outfile: './bitburner/bb-plasma.js',
+  plugins: [netscriptLoaderPlugin],
   minify: !WATCH,
   bundle: true,
   format: 'esm',
@@ -19,3 +52,4 @@ const ctx = await context({
 await ctx.rebuild();
 
 if (WATCH) ctx.watch();
+else ctx.dispose();
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,3 +7,14 @@ declare module '*.css' {
   const content: string;
   export default content;
 }
+
+declare module '*ns.ts' {
+  const content: string;
+  export default content;
+}
+
+
+declare module '*ns.tsx' {
+  const content: string;
+  export default content;
+}
