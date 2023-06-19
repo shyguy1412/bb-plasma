227d94f1798c264306475c07f72373a9d2fb637b
NilsRamstoeck <nilsramstoeck@gmail.com>
2023-06-19 21:52:08 +0200

    made windowmanager globally available

--- src/DesktopEnviroment.tsx
+++ src/DesktopEnviroment.tsx
@@ -1,12 +1,13 @@
 import { NS } from "@/types/NetscriptDefinitions";
 import { Taskbar } from "@/components/Taskbar";
 import { mapObject } from "@/lib/MapObject";
-import style from '@/style/DesktopEnviroment.css';
-import globalStyle from '@/style/global.css';
 import { WindowManagerReducer, WindowManager } from "@/lib/WindowManager";
 import { Desktop } from '@/components/Desktop';
 import { createContext, h } from "preact";
-import { useReducer } from "preact/hooks";
+import { useEffect, useReducer } from "preact/hooks";
+import { Program } from "@/lib/Program";
+import style from '@/style/DesktopEnviroment.css';
+import globalStyle from '@/style/global.css';
 
 type Props = {
   ns: NS;
@@ -18,6 +19,10 @@ export const TerminateContext = createContext<Partial<{ terminate: Props['termin
 export const RebootContext = createContext({ reboot: () => { } });
 export const WindowManagerContext = createContext<WindowManager>(null);
 
+export let WindowManagerInstance: WindowManager = [
+  { windows: [] },
+  () => { throw new Error('WindowManager is not available. Is BBPlasma running?'); }
+];
 
 export function DesktopEnviroment({ ns, terminate, reboot }: Props) {
 
@@ -25,9 +30,21 @@ export function DesktopEnviroment({ ns, terminate, reboot }: Props) {
     windows: []
   });
 
+  WindowManagerInstance = windowManager;
+
   const theme = mapObject(ns.ui.getTheme(), (key, value) => ({
     ['--' + key]: value
   }));
+
+  useEffect(() => {
+    return () => {
+      //Reset WindowManaferInstance
+      WindowManagerInstance = [
+        { windows: [] },
+        () => { throw new Error('WindowManager is not available. Is BBPlasma running?'); }
+      ];
+    };
+  }, []);
 
   return <div className='desktop-enviroment' style={theme}>
     <style>{style}{globalStyle}</style>
--- src/lib/Program.tsx
+++ src/lib/Program.tsx
@@ -1,31 +1,34 @@
-import { createWindow, PlasmaWindow } from "@/components/PlasmaWindow";
-import { WindowManagerState, WindowManagerDispatch, WindowManager } from "@/lib/WindowManager";
+import { createWindow } from "@/components/PlasmaWindow";
 import { FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
 import { h, JSX } from "preact";
+import { WindowManagerInstance } from '@/DesktopEnviroment';
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
-type NewType = {
+type ProgramOptions = {
   name: string;
-  icon: FontAwesomeIconProps['icon'];
+  icon?: FontAwesomeIconProps['icon'];
   WindowContents: () => JSX.Element;
+  options?: Parameters<typeof createWindow>[0];
 };
 
-type ProgramOptions = NewType;
-
 export class Program {
-  name: string;
-  icon: FontAwesomeIconProps['icon'];
-  launch: ([_, requestAction]: WindowManager) => void;
+  launch: () => void;
+  options: Parameters<typeof createWindow>[0];
 
-  constructor({ name, icon, WindowContents }: ProgramOptions) {
-    this.name = name;
-    this.icon = icon;
-    this.launch = ([_, requestAction]: WindowManager) => requestAction({
+  constructor({ name, icon, WindowContents, options }: ProgramOptions) {
+    
+    this.options = {
+      title: name,
+      icon: icon ?? faFile,
+      children: <WindowContents></WindowContents>,
+      ...options ?? {}
+    };
+    
+    const [_, requestAction] = WindowManagerInstance;
+    console.log(requestAction);
+    this.launch = () => requestAction({
       action: 'CREATE',
-      window: createWindow({
-        title: name,
-        icon: icon,
-        children: <WindowContents></WindowContents>
-      })
+      window: createWindow(this.options)
     });
 
   }
